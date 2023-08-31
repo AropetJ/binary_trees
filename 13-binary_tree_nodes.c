26495069c299b1e2712abf3ccd72b81fa6635884
AropetJ <aropetjoel32@gmail.com>
2023-08-31 17:41:20 +0300

    13-binary_tree_nodes.c - third commit

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -3,7 +3,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves - Counts the nodes with at least 1 child in a binary tree
+ * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree
  *
  * @tree: Pointer to the root node of the tree to count the number of nodes
  *
@@ -15,13 +15,8 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
-
-	else if (tree->left == NULL && tree->right == NULL)
-	{
-		return (0);
-	}
 
 	return (binary_tree_size(tree) - binary_tree_leaves(tree));
 }
