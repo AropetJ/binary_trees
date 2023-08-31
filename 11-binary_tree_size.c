70f383cfb63fa98c3bab53a92c48029448ce7d45
AropetJ <aropetjoel32@gmail.com>
2023-08-31 16:46:11 +0300

    11-binary_tree_size.c - first commit

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -12,15 +12,14 @@
  *
  * Return: Size of the binary tree.
  */
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return 0;
+	if (tree == NULL)
+		return (0);
 
-    // Recursively calculate the size of the left and right subtrees
-    size_t left_size = binary_tree_size(tree->left);
-    size_t right_size = binary_tree_size(tree->right);
+	size_t left_size = binary_tree_size(tree->left);
+	size_t right_size = binary_tree_size(tree->right);
 
-    // Return the sum of sizes of both subtrees plus 1 (for the current node)
-    return left_size + right_size + 1;
+	return (left_size + right_size + 1);
 }
